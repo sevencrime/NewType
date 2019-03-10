46f86d763b517fe26b007d1a1f48a229f3ba7bfc
sevencrime <onedi@qq.com>
2019-03-10 22:53:47 +0800

    reg_activity2

--- NewType/script/Lead/Reg_Activity.py
+++ NewType/script/Lead/Reg_Activity.py
@@ -24,10 +24,6 @@ class Reg_Activity():
             'x-api-key' : 'CCM4cuhy1e2r0XXkbzMuN1A5TiZ0tC6e6mJYKUez'
         }
 
-        # self.driver = webdriver.Chrome(executable_path = 'chromedriver')
-        # self.driver.implicitly_wait(30)
-        # self.driver.get('http://mail.tom.com/')
-        # self.driver.maximize_window()
 
     def get_List(self):
         url = self.path + '/public/leadDistribute/list'
@@ -47,8 +43,8 @@ class Reg_Activity():
         # print(resp['data']['rows'][0]['_id'])
         for lead in resp['data']['rows']:
             if lead['date'] >= datetime.datetime.now().strftime("%Y-%m-%d"):
-                print(lead)
-                return lead['_id']
+                # print(lead)
+                return lead
 
 
     def findSimilarLead(self, _id):
@@ -59,7 +55,7 @@ class Reg_Activity():
             "clientPhoneNumber": [],
             "phoneArr": [],
             "totalPeople": 1,
-            "email": "sevencrime77@tom.com",
+            "email": "sevencrime777@tom.com",
             "lastName": "Test",
             "firstName": "Test",
             "phoneNumber": 15088886666,
@@ -70,66 +66,78 @@ class Reg_Activity():
         resp = requests.post(url, headers=self.headers, data=json.dumps(data)).json()
         print(resp)
 
-    def login(self, username, password):
+        return data['totalPeople']
+
+    def login(self, username, password, lead):
+        driver = webdriver.Chrome(executable_path = 'chromedriver')
+        driver.implicitly_wait(30)
+        driver.get('http://mail.tom.com/')
+        driver.maximize_window()
         # 等待页面完全加载
-        self.driver.implicitly_wait(30)
+        driver.implicitly_wait(30)
 
         # 登录邮箱
-        user = self.driver.find_element_by_id("username").send_keys(username)
-        WebDriverWait(self.driver,20).until(EC.visibility_of_element_located((By.ID, "pw"))).click()
-        psw = self.driver.find_element_by_id("password").send_keys(password)
-        login = self.driver.find_element_by_class_name('mail_login_btn4').click()
-        assert self.driver.find_element_by_class_name('top-account').text == username+'@tom.com'
+        user = driver.find_element_by_id("username").send_keys(username)
+        WebDriverWait(driver,20).until(EC.visibility_of_element_located((By.ID, "pw"))).click()
+        psw = driver.find_element_by_id("password").send_keys(password)
+        login = driver.find_element_by_class_name('mail_login_btn4').click()
+        assert driver.find_element_by_class_name('top-account').text == username+'@tom.com'
 
         # 进入收件箱
-        self.driver.find_element_by_id('webmail_sys_readmail').click()
+        driver.find_element_by_id('webmail_sys_readmail').click()
 
-        # page = self.driver.page_source
-        # print(page.replace(u'\xa0', u''))
 
         # 爬虫,抓取收件箱列表
-        soup = BeautifulSoup(self.driver.page_source.replace(u'\xa0', u''), 'lxml')
+        soup = BeautifulSoup(driver.page_source.replace(u'\xa0', u''), 'lxml')
         maillist = soup.select('#main_tmpl_INBOX > .m-maillist > table > tbody > tr')
 
         # 循环遍历列表,找到邮件
         for i in range(len(maillist)):
-            print(maillist[i])
-            print(maillist[i]['title'])
+            # print(maillist[i])
+
+            # print(lead['name'])
             # 通过title初步判断
+            if lead['name'] in maillist[i]['title'].replace(' ', ''):
+                print('title')
+                # 获取发件人和收件时间
+                td_list = maillist[i].find_all('td')
+                for n in range(len(td_list)):
+                    if n == 2:
+                        assert td_list[n].get_text() == 'noreply'
+                    if n == 4:
+                        print(td_list[n].get_text())
+                        # 根据时间判断时候是刚收到的邮件,找不到重新刷新
+                        if (datetime.datetime.now()-datetime.timedelta(minutes=5)).strftime("%Y-%m-%d %H:%M:%S") <= td_list[n].get_text():
+                            print('date')
 
-            # if 活动名 in maillist[i]['title'] and 活动日期 in maillist[i]['title']:
-            # 获取发件人和收件时间
-            td_list = maillist[i].find_all('td')
-            for n in range(len(td_list)):
-                # if n == 2:
-                #     assert td_list[n].get_text() == 'noreply'
-                if n == 4:
-                    print(td_list[n].get_text())
-                    # 根据时间判断时候是刚收到的邮件
+                            print("ssssssssssssssss")
+                            print(maillist[i]['uid'])
+                            print("ssssssssssssssss")
+                            # 进入邮件
+                            driver.find_element_by_xpath(
+                                '//tr[@uid=%s]' %(maillist[i]['uid'])).click()
 
-                    # 进入邮件
-                    self.driver.find_element_by_xpath(
-                        '//tr[@uid=%s]' %(maillist[i]['uid'])).click()
-
-                    # 主题,讲者,日期,时间,预留名额断言
-                    # 截图,对比信息
-                    # 扫码
+                            # 主题,讲者,日期,时间,预留名额,地址断言
+                            # 截图,对比信息
+                            # 扫码
 
 
 
         time.sleep(5)
         print("quit driver")
-        self.driver.quit()
+        driver.quit()
 
 
 if __name__ == '__main__':
 
     reg = Reg_Activity()
-    _id = reg.get_List()
-    print(_id)
-    # reg.findSimilarLead(_id)
+    lead = reg.get_List()
+    print(lead)
+    totalPeople = reg.findSimilarLead(lead['_id'])
+    # print(totalPeople)
+
 
     username = "sevencrime77"
     password = "abcd1234"
-    # reg.login(username, password)
+    reg.login(username, password, lead)
 
