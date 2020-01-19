66bedb0fe6573730e5ca23f565f586ad40c22fed
sevencrime <onedi@qq.com>
2020-01-19 14:43:12 +0800

    del_link_mongo_new

--- NewType Interface/eddid-bos/Apply/create.py
+++ NewType Interface/eddid-bos/Apply/create.py
@@ -5,11 +5,11 @@ import json
 import random
 from requests.packages import urllib3
 
-url = 'https://eddid-api.ntdev.be/eddid-api-uat/apply/create'
+url = 'https://eddid-api.ntdev.be/eddid-api-esuat/apply/create'
 
 headers = {
     'Content-Type' : 'application/json' ,
-    'X-Token' : 'eyJraWQiOiJSejNcLzBrMzY0alZZK2NVVUQ4bWpjdEhYdHgrWTNROENNXC9FcG52OGhXbkE9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI3YWYzYWRhOS0yZmY5LTQ1MWQtODdkNy0xNjI5ZWVjZWQyNDMiLCJjb2duaXRvOmdyb3VwcyI6WyJhZG1pbiJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzpwcmVmZXJyZWRfcm9sZSI6ImFybjphd3M6aWFtOjo4MzI0MzE4NjQ2NjY6cm9sZVwvZGV2LWVkZGlkLWNvZ25pdG8tYWRtaW4tcm9sZSIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMV91OWZ6N2x5b04iLCJjb2duaXRvOnVzZXJuYW1lIjoiYWRtaW4iLCJnaXZlbl9uYW1lIjoidGVzdCIsImNvZ25pdG86cm9sZXMiOlsiYXJuOmF3czppYW06OjgzMjQzMTg2NDY2Njpyb2xlXC9kZXYtZWRkaWQtY29nbml0by1hZG1pbi1yb2xlIl0sImF1ZCI6IjUxM2pmY2t0cjFtNmV2b2dmcXU3b3NrN3BhIiwiZXZlbnRfaWQiOiJhMDEwMjUzMi00MDUzLTQ0MWQtYjRiZi1hNWE2OTE0NWJjNjUiLCJ0b2tlbl91c2UiOiJpZCIsImF1dGhfdGltZSI6MTU2NTA3ODE1OCwiZXhwIjoxNTY1MDgxNzU4LCJpYXQiOjE1NjUwNzgxNTgsImZhbWlseV9uYW1lIjoiYWRtaW4iLCJlbWFpbCI6ImFkbWluMTIzNEAxNjMuY29tIn0.Oe_nbIelIlwreA7dIx681Ei1oTM9nQ76_qtZ7zGX0sc5aFe4WPQeak8j60_jY0SIS08bt8oFH-jLcjxd545p5xRZslHww73Ueb_KVJkGgeJMeKBpUsqlGHxpLttGR_XcBy0v_4cVZTBOHZWLtz5dMVypIbVqPvCrok872jiLhF0N0ICZ4VrFu18dygCly4IoMP86Exvqrya8nFIZgL9PorlOFPwcr5UY_BFsl7B6h-zD9VDFzFkqBReV8iq6PPCx5UZds_Y279QFKjDz1VrZTRBDVlvmOErXmHlZU0wyMf8GpnXXOAkA3DRh2LFHnzy0FtL3wBM7Sw9BS06z32FMVg'
+    'X-Token' : 'eyJraWQiOiJSejNcLzBrMzY0alZZK2NVVUQ4bWpjdEhYdHgrWTNROENNXC9FcG52OGhXbkE9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJmZWYwOWU1MC03Y2NkLTQ0NmQtOWIwYS1hZjFhZDlkZTcwMTIiLCJjb2duaXRvOmdyb3VwcyI6WyJjczEiXSwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJjb2duaXRvOnByZWZlcnJlZF9yb2xlIjoiYXJuOmF3czppYW06OjgzMjQzMTg2NDY2Njpyb2xlXC9kZXYtZWRkaWQtY29nbml0by1hZG1pbi1yb2xlIiwiaXNzIjoiaHR0cHM6XC9cL2NvZ25pdG8taWRwLmFwLXNvdXRoZWFzdC0xLmFtYXpvbmF3cy5jb21cL2FwLXNvdXRoZWFzdC0xX3U5Zno3bHlvTiIsImNvZ25pdG86dXNlcm5hbWUiOiJjczFfb25lZGkiLCJnaXZlbl9uYW1lIjoidGlhbnlhbmciLCJjb2duaXRvOnJvbGVzIjpbImFybjphd3M6aWFtOjo4MzI0MzE4NjQ2NjY6cm9sZVwvZGV2LWVkZGlkLWNvZ25pdG8tYWRtaW4tcm9sZSJdLCJhdWQiOiI1MTNqZmNrdHIxbTZldm9nZnF1N29zazdwYSIsImV2ZW50X2lkIjoiYjU0MjdhNjItOWNiNS00YzY3LTgxMzEtYTRmOTMxYmMzODFlIiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1Njg2MjEzMTcsImV4cCI6MTU2ODYzMTU0MCwiaWF0IjoxNTY4NjI3OTQwLCJmYW1pbHlfbmFtZSI6IndhbmciLCJlbWFpbCI6IjE1MDg5NTE0NjI2QHNpbmEuY24ifQ.fLEZBepL4-wyeV3w-A7N-rNpm2yFVgGu3HWDYJJjCtT6yotdnTqAkGtpFnf7MrGEcT2CFcvjBZHPtlnOIq8-w9cwgeMzi8h4CpRWM75dWeEnDGRwK4YzmV7801I9tncfPogkNJU1jsdzqshcJrnpzFYtqy8Fg-_D5_RPN6j0IlUy8842VPzXdRFxC_WnB_xjGDirtuk63nYxNzgWj9IvHNCTnBkEG7mEfBb4Yg9r_8srOo8j4Se83NuXWRU4rbBDNHke1grtCBN0ClZpb9hs_4GtVOpbXOLNYl-HyClc9DxWHH9Jl3oXpiCin4A9g13l565gLVYDMhVpzxCFBmW_7Q'
 }
 
 data = {
@@ -22,14 +22,14 @@ data = {
     "customerSource" : "crm",
     "client": [{
         "title": "mr",
-        "firstName": "名",
-        "lastName": "姓",
+        "firstName": "first",
+        "lastName": "last",
         "chineseName": "姓名",
         # "email": "%sonedi%s@qq.com" %(random.randint(1,10235), random.randint(1,1562535)),
-        "email": "flynn.shu@newtype.com", 
+        "email": "onedi@qq.com", 
         "phoneAreaCode": "CHN",
         # "phone": "322%s546" %(random.randint(1,10235)),
-        "phone": "18218805466",
+        "phone": "15089514626",
         "address": "465456456456",
         # "nationality": "HKG",
         "nationality": "CHN",
--- NewType Interface/eddid-bos/public/clientPortalNotification.py
+++ NewType Interface/eddid-bos/public/clientPortalNotification.py
@@ -5,16 +5,47 @@
 # @Link    : ${link}
 # @Version : $Id$
 
-import requests
-import json
+# import requests
+# import json
 
-url = 'https://eddid-api.ntdev.be/eddid-api-uat/client/clientPortalNotification'
-headers = {
-    'Content-Type' : 'application/json' ,
-    'X-Token' : 'eyJraWQiOiJSejNcLzBrMzY0alZZK2NVVUQ4bWpjdEhYdHgrWTNROENNXC9FcG52OGhXbkE9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI3YWYzYWRhOS0yZmY5LTQ1MWQtODdkNy0xNjI5ZWVjZWQyNDMiLCJjb2duaXRvOmdyb3VwcyI6WyJhZG1pbiJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzpwcmVmZXJyZWRfcm9sZSI6ImFybjphd3M6aWFtOjo4MzI0MzE4NjQ2NjY6cm9sZVwvZGV2LWVkZGlkLWNvZ25pdG8tYWRtaW4tcm9sZSIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMV91OWZ6N2x5b04iLCJjb2duaXRvOnVzZXJuYW1lIjoiYWRtaW4iLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJjb2duaXRvOnJvbGVzIjpbImFybjphd3M6aWFtOjo4MzI0MzE4NjQ2NjY6cm9sZVwvZGV2LWVkZGlkLWNvZ25pdG8tYWRtaW4tcm9sZSJdLCJhdWQiOiI1MTNqZmNrdHIxbTZldm9nZnF1N29zazdwYSIsImV2ZW50X2lkIjoiMGUwNDcxZTMtNGExZC0xMWU5LWE0NjAtYzM3N2M5MzRlYWU0IiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1NTI5ODI0MjYsImV4cCI6MTU1Mjk5NDIzMywiaWF0IjoxNTUyOTkwNjMzLCJmYW1pbHlfbmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbjEyM0BxcS5jb20ifQ.ZbfZkl5R39wyAtOHqM4TMYDMW5Q0aI_HoSdewIlfPFwhWd_NbyvPbELPswvStEtpyRo3wJr1u7EOR9fu8gkGTbFm4E2RSLxboRAz1WmrZdtzQC3yEEOIS6fN9XbPbAVyW8KoEzEi71I5NyTXaQtNU5RwD1gt1aNvv_AfBJ-dOEjVhEfXwvMrQCutO1kqJKVazf4g8EpSC3JGDHpcMkLY5PqiIunMpPNDx7JiCDa_XqgMWkc71UgFoLDJlZWAf9tzSQ_Dgw0-rSPlRawI0PIH0c12xaznDcjvImNa0qCJuLQs0WdHf6FxG_oZpcAsgLj8jGOvNK9-I5SfaFLodQ9jjA'
-}
+# url = 'https://eddid-api.ntdev.be/eddid-api-uat/client/clientPortalNotification'
+# headers = {
+#     'Content-Type' : 'application/json' ,
+#     'X-Token' : 'eyJraWQiOiJSejNcLzBrMzY0alZZK2NVVUQ4bWpjdEhYdHgrWTNROENNXC9FcG52OGhXbkE9IiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI3YWYzYWRhOS0yZmY5LTQ1MWQtODdkNy0xNjI5ZWVjZWQyNDMiLCJjb2duaXRvOmdyb3VwcyI6WyJhZG1pbiJdLCJlbWFpbF92ZXJpZmllZCI6dHJ1ZSwiY29nbml0bzpwcmVmZXJyZWRfcm9sZSI6ImFybjphd3M6aWFtOjo4MzI0MzE4NjQ2NjY6cm9sZVwvZGV2LWVkZGlkLWNvZ25pdG8tYWRtaW4tcm9sZSIsImlzcyI6Imh0dHBzOlwvXC9jb2duaXRvLWlkcC5hcC1zb3V0aGVhc3QtMS5hbWF6b25hd3MuY29tXC9hcC1zb3V0aGVhc3QtMV91OWZ6N2x5b04iLCJjb2duaXRvOnVzZXJuYW1lIjoiYWRtaW4iLCJnaXZlbl9uYW1lIjoiYWRtaW4iLCJjb2duaXRvOnJvbGVzIjpbImFybjphd3M6aWFtOjo4MzI0MzE4NjQ2NjY6cm9sZVwvZGV2LWVkZGlkLWNvZ25pdG8tYWRtaW4tcm9sZSJdLCJhdWQiOiI1MTNqZmNrdHIxbTZldm9nZnF1N29zazdwYSIsImV2ZW50X2lkIjoiMGUwNDcxZTMtNGExZC0xMWU5LWE0NjAtYzM3N2M5MzRlYWU0IiwidG9rZW5fdXNlIjoiaWQiLCJhdXRoX3RpbWUiOjE1NTI5ODI0MjYsImV4cCI6MTU1Mjk5NDIzMywiaWF0IjoxNTUyOTkwNjMzLCJmYW1pbHlfbmFtZSI6ImFkbWluIiwiZW1haWwiOiJhZG1pbjEyM0BxcS5jb20ifQ.ZbfZkl5R39wyAtOHqM4TMYDMW5Q0aI_HoSdewIlfPFwhWd_NbyvPbELPswvStEtpyRo3wJr1u7EOR9fu8gkGTbFm4E2RSLxboRAz1WmrZdtzQC3yEEOIS6fN9XbPbAVyW8KoEzEi71I5NyTXaQtNU5RwD1gt1aNvv_AfBJ-dOEjVhEfXwvMrQCutO1kqJKVazf4g8EpSC3JGDHpcMkLY5PqiIunMpPNDx7JiCDa_XqgMWkc71UgFoLDJlZWAf9tzSQ_Dgw0-rSPlRawI0PIH0c12xaznDcjvImNa0qCJuLQs0WdHf6FxG_oZpcAsgLj8jGOvNK9-I5SfaFLodQ9jjA'
+# }
 
 
-resp = requests.post(url, headers = headers, verify=False)
-print(resp)
-print(resp.json())
+# resp = requests.post(url, headers = headers, verify=False)
+# print(resp)
+# print(resp.json())
+
+
+import sys
+import inspect
+ 
+def my_name():
+    print ('1' ,sys._getframe().f_code.co_name)
+    print ('2' ,inspect.stack()[0][3])
+ 
+def get_current_function_name():
+    print ('5', sys._getframe().f_code.co_name)
+    return inspect.stack()[1][3]
+class MyClass:
+    def function_one(self):
+        print ('3',inspect.stack()[0][3])
+        print ('4', sys._getframe().f_code.co_name)
+        print ("6 %s.%s invoked"%(self.__class__.__name__, get_current_function_name()))
+ 
+if __name__ == '__main__':
+    my_name()
+    myclass = MyClass()
+    myclass.function_one()
+
+
+
+
+
+
+
+
+
